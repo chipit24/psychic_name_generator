3e14bb754528f6604b4317d64bec34e1685bb858
Robert Komaromi <robert.komaromi@vidyard.com>
2015-10-18 20:47:44 -0400

    Trying to make things work...

--- app.js
+++ app.js
@@ -1,57 +1,55 @@
-// (function () {
-  'use strict';
+'use strict';
 
-  // Set up our word dictionaries
-  var adjectives = [
-    'Spiritual',
-    'Esoteric',
-    'Solar',
-    'Natural',
-    'Astral',
-    'Shimmering',
-    'Angelic',
-    'Holy',
-    'Prophetic',
-    'Serendipitous',
-    'Magical',
-    'Psychic',
-    'Open',
-    'Mindful',
-    'Peaceful',
-    'White'
-  ];
+// Set up our word dictionaries
+var adjectives = [
+  'Spiritual',
+  'Esoteric',
+  'Solar',
+  'Natural',
+  'Astral',
+  'Shimmering',
+  'Angelic',
+  'Holy',
+  'Prophetic',
+  'Serendipitous',
+  'Magical',
+  'Psychic',
+  'Open',
+  'Mindful',
+  'Peaceful',
+  'White'
+];
 
-  var nouns = [
-    'moon',
-    'light',
-    'sun',
-    'star',
-    'planet',
-    'flower',
-    'rain'
-  ];
+var nouns = [
+  'moon',
+  'light',
+  'sun',
+  'star',
+  'planet',
+  'flower',
+  'rain'
+];
 
-  var types = [
-    'walker',
-    'angel',
-    'spirit',
-    'fairy',
-    'gem',
-    'unicorn',
-    'butterfly',
-  ];
+var types = [
+  'walker',
+  'angel',
+  'spirit',
+  'fairy',
+  'gem',
+  'unicorn',
+  'butterfly',
+];
 
-  var generateName = function (e) {
-    e.preventDefault(); 
+var generateName = function (e) {
+  e.preventDefault(); 
 
-    var name = adjectives[Math.floor(Math.random() * adjectives.length)] + ' ' +
-      nouns[Math.floor(Math.random() * nouns.length)] + ' ' +
-      types[Math.floor(Math.random() * types.length)];
+  var name = adjectives[Math.floor(Math.random() * adjectives.length)] + ' ' +
+    nouns[Math.floor(Math.random() * nouns.length)] + ' ' +
+    types[Math.floor(Math.random() * types.length)];
 
-    // Generated names will be of the form adjective + noun + type
-    document.getElementById('name').value = name;
+  // Generated names will be of the form adjective + noun + type
+  document.getElementById('name').value = name;
 
-    // Say the name
-    responsiveVoice.speak(name);
-  }
-})();
+  // Say the name
+  responsiveVoice.speak(name);
+};
